82a61f127e4b0368cfa36a0836940c7a9710318f
Ellis <elnobun@gmail.com>
2021-03-25 02:11:48 +0000

    server code refactoring

--- server/server.js
+++ server/server.js
@@ -16,5 +16,5 @@ app.use(express.json())
 app.use("/api/products", productRoutes)
 
 // Application should listen on a port 5000
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
